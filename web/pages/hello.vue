918335a90a76802db6d461816174c4a638aa23f1
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-08-09 16:44:30 +0900

    Add sample for request with type

--- web/pages/hello.vue
+++ web/pages/hello.vue
@@ -5,28 +5,35 @@
     </v-flex>
     <v-btn rounded @click="getHello">get</v-btn>
     <v-btn rounded @click="postHello">post</v-btn>
+    <v-btn rounded @click="getDraft">draft</v-btn>
   </v-layout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
+import { Draft } from '~/types'
 
 @Component({
   async asyncData({ $axios }) {
-    const { data } = await $axios.get('/api/hello')
+    const { data } = await $axios.get<string>('/api/hello')
     return { message: data }
   }
 })
 export default class extends Vue {
   message: string = ''
   private getHello(): void {
-    this.$axios.get('/api/hello').then(response => {
+    this.$axios.get<string>('/api/hello').then(response => {
       this.message = response.data
     })
   }
   private postHello(): void {
-    this.$axios.post('/api/hello').then(response => {
+    this.$axios.post<string>('/api/hello').then(response => {
       this.message = response.data
+    })
+  }
+  private getDraft(): void {
+    this.$axios.get<Draft>('/api/draft/2').then(response => {
+      this.message = response.data.description
     })
   }
   private head() {
