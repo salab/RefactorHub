86a34aa4dae22a91dad0f9c6bd644bdc3c29456d
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-09-01 10:16:36 +0900

    Fix linter error

--- web/plugins/axios.ts
+++ web/plugins/axios.ts
@@ -4,12 +4,4 @@ export default ({ $axios }: Context): void => {
   $axios.defaults.withCredentials = true
   $axios.defaults.xsrfCookieName = 'XSRF-TOKEN'
   $axios.defaults.xsrfHeaderName = 'X-XSRF-TOKEN'
-
-  $axios.onRequest((config): void => {
-    console.log(`${config.method} ${config.url}`)
-  })
-
-  $axios.onError((error): void => {
-    console.log(`${error.name}: ${error.message}`)
-  })
 }
