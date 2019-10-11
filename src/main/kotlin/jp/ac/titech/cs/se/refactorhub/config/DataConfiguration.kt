5d3a496e4b16e1f9ed241d75ab6a9fc36e2dcb46
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-10-12 00:58:27 +0900

    Add initial data

--- src/main/kotlin/jp/ac/titech/cs/se/refactorhub/config/DataConfiguration.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/refactorhub/config/DataConfiguration.kt
@@ -23,16 +23,18 @@ class DataConfiguration(
     fun initializeDevData() = InitializingBean {
         val admin = userService.create(1, "admin")
         val users = (2..4).map { userService.create(it, "user-$it") }
-        val commit =
-            commitService.create("f35b2c8eb8c320f173237e44d04eefb4634649a2", "danilofes", "refactoring-toy-example")
-        val annotation = annotationService.save(
-            Annotation(
-                admin,
-                commit,
-                description = "This is a sample of ExtractMethod."
+        listOf(
+            commitService.create("f35b2c8eb8c320f173237e44d04eefb4634649a2", "danilofes", "refactoring-toy-example"),
+            commitService.create("7655200f58293e5a30bf8b3cbb29ebadae374564", "JetBrains", "intellij-community")
+        ).forEach { commit ->
+            val annotation = annotationService.save(
+                Annotation(
+                    admin,
+                    commit,
+                    description = "This is a sample of ExtractMethod."
+                )
             )
-        )
-        users.forEach { draftService.fork(it, annotation) }
+            users.forEach { draftService.fork(it, annotation) }
+        }
     }
-
 }
