307ff25ad4a240564e22b7c70edb83281047e706
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-11-04 23:59:29 +0900

    Fix error page

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -1,31 +1,29 @@
 <template>
-  <v-layout justify-center align-center>
-    <v-flex v-if="error.statusCode === 404" display-1 text-xs-center>
-      {{ pageNotFound }}
-    </v-flex>
-    <v-flex v-else display-1 text-xs-center>
-      {{ otherError }}
-    </v-flex>
-  </v-layout>
+  <v-container fluid fill-height>
+    <v-row>
+      <v-col>
+        <v-row justify="center">
+          <p class="display-4">
+            {{ error.statusCode }}
+          </p>
+        </v-row>
+        <v-row justify="center">
+          <p class="headline">
+            {{ error.message }}
+          </p>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { NuxtError } from '@nuxt/types'
 
-@Component({
-  layout: 'empty'
-})
+@Component
 export default class extends Vue {
-  private head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return { title }
-  }
-
-  @Prop({ type: Object, required: true })
-  error!: { statusCode: number }
-
-  pageNotFound = '404 Not Found'
-  otherError = 'An error occurred'
+  @Prop({ required: true })
+  error!: NuxtError
 }
 </script>
