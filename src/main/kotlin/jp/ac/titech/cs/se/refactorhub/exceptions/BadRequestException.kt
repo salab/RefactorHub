8130af32761e6487a55e882be0ca04172a6ff4f6
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-12-10 12:36:36 +0900

    Fix imports

--- src/main/kotlin/jp/ac/titech/cs/se/refactorhub/exceptions/BadRequestException.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/refactorhub/exceptions/BadRequestException.kt
@@ -2,7 +2,6 @@ package jp.ac.titech.cs.se.refactorhub.exceptions
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
-import java.lang.RuntimeException
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 class BadRequestException(message: String) : RuntimeException(message)
--- src/main/kotlin/jp/ac/titech/cs/se/refactorhub/exceptions/UnauthorizedException.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/refactorhub/exceptions/UnauthorizedException.kt
@@ -2,7 +2,6 @@ package jp.ac.titech.cs.se.refactorhub.exceptions
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
-import java.lang.RuntimeException
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 class UnauthorizedException(message: String) : RuntimeException(message)
