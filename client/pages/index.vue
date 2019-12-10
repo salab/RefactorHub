0323e74d1858d901eaa78574bce1fc41b74167fe
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-12-10 14:10:14 +0900

    Add show init data

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -3,8 +3,18 @@
     <v-row>
       <v-col>
         <v-row align="center" justify="center">
+          <div v-if="$auth.loggedIn">Welcome, {{ $auth.user.name }}</div>
+          <v-btn v-else @click="$auth.loginWith('github')">
+            <v-icon left>fab fa-github</v-icon> Login
+          </v-btn>
+        </v-row>
+        <v-divider class="my-4" />
+        <v-row align="center" justify="center">
+          <p class="subtitle-1">Tutorials</p>
+        </v-row>
+        <v-row align="center" justify="center">
           <v-btn
-            v-for="refactoring in refactorings"
+            v-for="refactoring in tutorials"
             :key="refactoring.id"
             class="text-capitalize mx-2"
             @click="fork(refactoring.id)"
@@ -14,9 +24,16 @@
         </v-row>
         <v-divider class="my-4" />
         <v-row align="center" justify="center">
-          <div v-if="$auth.loggedIn">Welcome, {{ $auth.user.name }}</div>
-          <v-btn v-else @click="$auth.loginWith('github')">
-            <v-icon left>fab fa-github</v-icon> Login
+          <p class="subtitle-1">Experiments</p>
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-btn
+            v-for="refactoring in experiments"
+            :key="refactoring.id"
+            class="text-capitalize mx-2"
+            @click="fork(refactoring.id)"
+          >
+            {{ refactoring.type.name }} ({{ refactoring.id }})
           </v-btn>
         </v-row>
       </v-col>
@@ -30,10 +47,12 @@ import { Refactoring } from 'refactorhub'
 
 @Component
 export default class extends Vue {
-  refactorings: Refactoring[] = []
+  tutorials: Refactoring[] = []
+  experiments: Refactoring[] = []
 
   async mounted() {
-    this.refactorings = await this.$client.getRefactorings()
+    this.tutorials = await this.$client.getUserRefactorings(1)
+    this.experiments = await this.$client.getUserRefactorings(2)
   }
 
   async fork(id: number) {
