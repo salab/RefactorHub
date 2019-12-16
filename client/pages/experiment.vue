266f321bb3ca7f50eb741f889f3dbc47730d41ac
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2019-12-17 00:39:36 +0900

    Impl experiment page

---
+++ client/pages/experiment.vue
@@ -0,0 +1,99 @@
+<template>
+  <v-container fill-height>
+    <v-col>
+      <v-row>
+        <div class="headline">Experiments</div>
+      </v-row>
+      <v-row>
+        <v-col v-for="experiment in experiments" :key="experiment.id" cols="12">
+          <v-row>
+            <v-divider class="mb-4" />
+          </v-row>
+          <v-row align="center">
+            <div class="title">
+              {{ experiment.type.name }}(id={{ experiment.id }})
+            </div>
+            <v-btn class="text-none ml-2" @click="fork(experiment.id)">
+              <v-icon left>fa-code-branch</v-icon>
+              Fork
+            </v-btn>
+          </v-row>
+          <v-row>
+            <div class="subtitle-1">My Refactorings</div>
+          </v-row>
+          <v-row>
+            <div
+              v-for="refactoring in getRefactorings(experiment.id)"
+              :key="refactoring.id"
+              class="ma-2"
+            >
+              <v-btn class="text-none" @click="edit(refactoring.id)">
+                {{ refactoring.type.name }}(id={{ refactoring.id
+                }}{{
+                  refactoring.parent ? `, parent=${refactoring.parent.id}` : ''
+                }})
+              </v-btn>
+            </div>
+          </v-row>
+          <v-row>
+            <div class="subtitle-1">My Drafts</div>
+          </v-row>
+          <v-row>
+            <div
+              v-for="draft in getDrafts(experiment.id)"
+              :key="draft.id"
+              class="ma-2"
+            >
+              <v-btn class="text-none" @click="open(draft.id)">
+                {{ draft.type.name }}(id={{ draft.id
+                }}{{ draft.parent ? `, parent=${draft.parent.id}` : ''
+                }}{{ draft.origin ? `, origin=${draft.origin.id}` : '' }})
+              </v-btn>
+            </div>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import { Refactoring, Draft, User } from 'refactorhub'
+
+@Component
+export default class extends Vue {
+  private experiments: Refactoring[] = []
+  private refactorings: Refactoring[] = []
+  private drafts: Draft[] = []
+
+  private async mounted() {
+    this.experiments = await this.$client.getUserRefactorings(2)
+    const user = this.$auth.user as User
+    this.refactorings = await this.$client.getUserRefactorings(user.id)
+    this.drafts = await this.$client.getUserDrafts(user.id)
+  }
+
+  private async fork(id: number) {
+    const draft = await this.$client.forkRefactoring(id)
+    this.$router.push(`/draft/${draft.id}`)
+  }
+
+  private async edit(id: number) {
+    const draft = await this.$client.editRefactoring(id)
+    this.$router.push(`/draft/${draft.id}`)
+  }
+
+  private open(id: number) {
+    this.$router.push(`/draft/${id}`)
+  }
+
+  private getRefactorings(id: number) {
+    return this.refactorings.filter(it => it.parent?.id === id)
+  }
+
+  private getDrafts(id: number) {
+    return this.drafts.filter(it => it.parent?.id === id)
+  }
+}
+</script>
