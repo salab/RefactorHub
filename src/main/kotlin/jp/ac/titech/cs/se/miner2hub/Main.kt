38a7177e8dc9f91f3b3941508924ec34f1258930
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-06-17 11:46:03 +0900

    feat: Impl output dataset as ndjson

--- src/main/kotlin/jp/ac/titech/cs/se/miner2hub/Main.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/miner2hub/Main.kt
@@ -4,12 +4,20 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import jp.ac.titech.cs.se.miner2hub.converter.convert
 import jp.ac.titech.cs.se.miner2hub.miner.Miner
 import jp.ac.titech.cs.se.miner2hub.oracle.Oracle
+import java.io.File
+import java.io.FileWriter
+
+const val PATH = "outputs/dataset.ndjson"
 
 fun main(args: Array<String>) {
+    val file = File(PATH)
+    if (!file.exists()) file.createNewFile()
     val dataset = Oracle.getDataset("Extract Method", 1, 1, false)
     dataset.forEach { metadata ->
         Miner.reDetect(metadata) {
-            println(jacksonObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(convert(it, metadata)))
+            FileWriter(file, true).use { out ->
+                out.appendln(jacksonObjectMapper().writeValueAsString(convert(it, metadata)))
+            }
         }
     }
 }
