ac08daa99ab3371d4bcaa1689afe32804310c203
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-06-22 18:53:45 +0900

    fix: Fix add type request is not worked

--- src/main/kotlin/jp/ac/titech/cs/se/refactorhub/controllers/refactoring/RefactoringController.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/refactorhub/controllers/refactoring/RefactoringController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -41,6 +42,6 @@ class RefactoringController(
     fun types() = refactoringTypeService.getAll()
 
     @PutMapping("/types")
-    fun addType(request: AddRefactoringTypeRequest) =
+    fun addType(@RequestBody request: AddRefactoringTypeRequest) =
         refactoringTypeService.create(request.name, request.before, request.after)
 }
--- src/main/kotlin/jp/ac/titech/cs/se/refactorhub/controllers/refactoring/requests/AddRefactoringTypeRequest.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/refactorhub/controllers/refactoring/requests/AddRefactoringTypeRequest.kt
@@ -3,8 +3,8 @@ package jp.ac.titech.cs.se.refactorhub.controllers.refactoring.requests
 import jp.ac.titech.cs.se.refactorhub.models.element.Element
 
 data class AddRefactoringTypeRequest(
-    val name: String = "",
-    val before: Map<String, Element.Info> = mapOf(),
-    val after: Map<String, Element.Info> = mapOf()
+    val name: String,
+    val before: Map<String, Element.Info>,
+    val after: Map<String, Element.Info>
 )
 
