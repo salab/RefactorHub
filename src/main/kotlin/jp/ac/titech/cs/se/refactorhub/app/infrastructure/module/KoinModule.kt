f678b41a8261c1f2a4ac25ff68c85aea928fd870
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-07-10 14:41:23 +0900

    feat: Add Koin modules

--- src/main/kotlin/jp/ac/titech/cs/se/refactorhub/app/infrastructure/module/KoinModule.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/refactorhub/app/infrastructure/module/KoinModule.kt
@@ -3,6 +3,11 @@ package jp.ac.titech.cs.se.refactorhub.app.infrastructure.module
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import jp.ac.titech.cs.se.refactorhub.app.infrastructure.db.repository.UserRepositoryImpl
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.CommitController
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.ElementController
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.RefactoringController
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.RefactoringDraftController
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.RefactoringTypeController
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.UserController
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.repository.UserRepository
 import jp.ac.titech.cs.se.refactorhub.app.usecase.service.UserService
@@ -12,6 +17,11 @@ import org.koin.dsl.module
 val koinModules = module {
     single { jacksonObjectMapper() }
     single { UserController() }
+    single { CommitController() }
+    single { RefactoringController() }
+    single { RefactoringTypeController() }
+    single { RefactoringDraftController() }
+    single { ElementController() }
     single { UserService() }
     single<UserRepository> { UserRepositoryImpl() }
 }
