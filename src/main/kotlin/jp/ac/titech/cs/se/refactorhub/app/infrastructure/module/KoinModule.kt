5f0d05b5e8cf1761890f78c61833b7c6fd230776
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-07-15 23:26:25 +0900

    feat: Add modules

--- src/main/kotlin/jp/ac/titech/cs/se/refactorhub/app/infrastructure/module/KoinModule.kt
+++ src/main/kotlin/jp/ac/titech/cs/se/refactorhub/app/infrastructure/module/KoinModule.kt
@@ -2,6 +2,10 @@ package jp.ac.titech.cs.se.refactorhub.app.infrastructure.module
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.locations.KtorExperimentalLocationsAPI
+import jp.ac.titech.cs.se.refactorhub.app.infrastructure.db.repository.CommitRepositoryImpl
+import jp.ac.titech.cs.se.refactorhub.app.infrastructure.db.repository.RefactoringDraftRepositoryImpl
+import jp.ac.titech.cs.se.refactorhub.app.infrastructure.db.repository.RefactoringRepositoryImpl
+import jp.ac.titech.cs.se.refactorhub.app.infrastructure.db.repository.RefactoringTypeRepositoryImpl
 import jp.ac.titech.cs.se.refactorhub.app.infrastructure.db.repository.UserRepositoryImpl
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.CommitController
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.ElementController
@@ -9,7 +13,16 @@ import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.RefactoringContr
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.RefactoringDraftController
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.RefactoringTypeController
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.controller.UserController
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.repository.CommitRepository
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.repository.RefactoringDraftRepository
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.repository.RefactoringRepository
+import jp.ac.titech.cs.se.refactorhub.app.interfaces.repository.RefactoringTypeRepository
 import jp.ac.titech.cs.se.refactorhub.app.interfaces.repository.UserRepository
+import jp.ac.titech.cs.se.refactorhub.app.usecase.service.CommitService
+import jp.ac.titech.cs.se.refactorhub.app.usecase.service.ElementService
+import jp.ac.titech.cs.se.refactorhub.app.usecase.service.RefactoringDraftService
+import jp.ac.titech.cs.se.refactorhub.app.usecase.service.RefactoringService
+import jp.ac.titech.cs.se.refactorhub.app.usecase.service.RefactoringTypeService
 import jp.ac.titech.cs.se.refactorhub.app.usecase.service.UserService
 import org.koin.dsl.module
 
@@ -23,5 +36,14 @@ val koinModules = module {
     single { RefactoringDraftController() }
     single { ElementController() }
     single { UserService() }
+    single { CommitService() }
+    single { RefactoringService() }
+    single { RefactoringTypeService() }
+    single { RefactoringDraftService() }
+    single { ElementService() }
     single<UserRepository> { UserRepositoryImpl() }
+    single<CommitRepository> { CommitRepositoryImpl() }
+    single<RefactoringRepository> { RefactoringRepositoryImpl() }
+    single<RefactoringTypeRepository> { RefactoringTypeRepositoryImpl() }
+    single<RefactoringDraftRepository> { RefactoringDraftRepositoryImpl() }
 }
