90184ac4dcd07ef43038f32688b62d06d80f5d42
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-07-20 18:52:32 +0900

    fix: Fix migration sql (add missing semi colon)

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -1,28 +1,35 @@
-CREATE TABLE commits (
+DROP TABLE IF EXISTS refactoring_drafts;
+DROP TABLE IF EXISTS refactoring_to_refactorings;
+DROP TABLE IF EXISTS refactorings;
+DROP TABLE IF EXISTS refactoring_types;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS commits;
+
+CREATE TABLE IF NOT EXISTS commits (
     id SERIAL PRIMARY KEY,
     sha VARCHAR(40) NOT NULL,
     "owner" VARCHAR(100) NOT NULL,
     repository VARCHAR(100) NOT NULL
-)
-ALTER TABLE commits ADD CONSTRAINT commits_sha_unique UNIQUE (sha)
+);
+ALTER TABLE commits ADD CONSTRAINT commits_sha_unique UNIQUE (sha);
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     sub_id INT NOT NULL,
     "name" VARCHAR(50) NOT NULL
-)
-ALTER TABLE users ADD CONSTRAINT users_sub_id_unique UNIQUE (sub_id)
+);
+ALTER TABLE users ADD CONSTRAINT users_sub_id_unique UNIQUE (sub_id);
 
-CREATE TABLE refactoring_types (
+CREATE TABLE IF NOT EXISTS refactoring_types (
     id SERIAL PRIMARY KEY,
     "name" VARCHAR(100) NOT NULL,
     "before" JSONB NOT NULL,
     "after" JSONB NOT NULL,
     description TEXT NOT NULL
-)
-ALTER TABLE refactoring_types ADD CONSTRAINT refactoring_types_name_unique UNIQUE ("name")
+);
+ALTER TABLE refactoring_types ADD CONSTRAINT refactoring_types_name_unique UNIQUE ("name");
 
-CREATE TABLE refactorings (
+CREATE TABLE IF NOT EXISTS refactorings (
     id SERIAL PRIMARY KEY,
     "owner" INT NOT NULL,
     "commit" INT NOT NULL,
@@ -32,16 +39,16 @@ CREATE TABLE refactorings (
     CONSTRAINT fk_refactorings_owner_id FOREIGN KEY ("owner") REFERENCES users(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT fk_refactorings_commit_id FOREIGN KEY ("commit") REFERENCES commits(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT fk_refactorings_type_id FOREIGN KEY ("type") REFERENCES refactoring_types(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
+);
 
-CREATE TABLE refactoringtorefactorings (
+CREATE TABLE IF NOT EXISTS refactoring_to_refactorings (
     parent_refactoring_id INT NOT NULL,
     child_refactoring_id INT NOT NULL,
-    CONSTRAINT fk_refactoringtorefactorings_parent_refactoring_id_id FOREIGN KEY (parent_refactoring_id) REFERENCES refactorings(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    CONSTRAINT fk_refactoringtorefactorings_child_refactoring_id_id FOREIGN KEY (child_refactoring_id) REFERENCES refactorings(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
+    CONSTRAINT fk_refactoring_to_refactorings_parent_refactoring_id_id FOREIGN KEY (parent_refactoring_id) REFERENCES refactorings(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT fk_refactoring_to_refactorings_child_refactoring_id_id FOREIGN KEY (child_refactoring_id) REFERENCES refactorings(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+);
 
-CREATE TABLE refactoring_drafts (
+CREATE TABLE IF NOT EXISTS refactoring_drafts (
     id SERIAL PRIMARY KEY,
     "owner" INT NOT NULL,
     origin INT NOT NULL,
@@ -54,4 +61,4 @@ CREATE TABLE refactoring_drafts (
     CONSTRAINT fk_refactoring_drafts_origin_id FOREIGN KEY (origin) REFERENCES refactorings(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT fk_refactoring_drafts_commit_id FOREIGN KEY ("commit") REFERENCES commits(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
     CONSTRAINT fk_refactoring_drafts_type_id FOREIGN KEY ("type") REFERENCES refactoring_types(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-)
+);
