bee22516b10be0a1c2f4cbed64ba9701abb3ca22
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-11-13 18:07:24 +0900

    feat: Improve root page

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,15 +1,31 @@
 <template>
-  <v-container fill-height>
-    <v-col>
-      <link-buttons />
-    </v-col>
+  <v-container>
+    <div class="pa-5 text-center">
+      <h1 class="display-3">RefactorHub</h1>
+    </div>
+    <v-divider />
+    <v-row justify="center" class="pa-5">
+      <div v-for="link in links" :key="link.to" class="ma-3">
+        <v-btn :to="link.to" class="text-none">
+          {{ link.name }}
+        </v-btn>
+      </div>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 
+const links = [
+  { name: 'All Refactorings', to: '/all' },
+  { name: 'Experiments', to: '/experiment' },
+]
+
 export default defineComponent({
   name: 'home',
+  setup() {
+    return { links }
+  },
 })
 </script>
