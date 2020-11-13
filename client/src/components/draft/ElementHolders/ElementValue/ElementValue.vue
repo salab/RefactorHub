cbfba121c4350551c18caa300396e051eaa99696
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-11-13 14:25:36 +0900

    feat: Improve location.path view

--- client/src/components/draft/ElementHolders/ElementValue/ElementValue.vue
+++ client/src/components/draft/ElementHolders/ElementValue/ElementValue.vue
@@ -1,9 +1,9 @@
 <template>
   <div :class="{ [`element-value-${element.type}`]: isEditing }" class="d-flex">
-    <div class="flex-grow-1 d-flex flex-column justify-center">
-      <div class="d-flex justify-center">
-        <span class="caption text--secondary" :title="path">{{
-          trimFileName(path, 20)
+    <div class="location flex-grow-1 d-flex flex-column justify-center pa-2">
+      <div class="path d-flex justify-center">
+        <span ref="pathRef" class="caption text--secondary" :title="path">{{
+          path
         }}</span>
       </div>
       <element-range :range="range" />
@@ -24,7 +24,6 @@
         icon
         title="Start to select on editor"
         color="primary"
-        class="mt-1"
         @click="toggleEditLocation"
       >
         <v-icon x-small>fa-fw fa-pen</v-icon>
@@ -35,7 +34,6 @@
         icon
         title="Delete"
         color="error"
-        class="mt-1"
         @click="deleteLocation"
       >
         <v-icon x-small>fa-fw fa-trash</v-icon>
@@ -45,7 +43,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, useContext } from '@nuxtjs/composition-api'
+import {
+  defineComponent,
+  computed,
+  useContext,
+  onMounted,
+  ref,
+} from '@nuxtjs/composition-api'
 import { DiffCategory } from 'refactorhub'
 import { trimFileName } from '@/components/common/editor/utils/trim'
 import { deleteElementDecoration } from '@/components/draft/ElementEditor/ts/elementDecorations'
@@ -82,6 +86,11 @@ export default defineComponent({
     const {
       app: { $accessor },
     } = useContext()
+
+    const pathRef = ref<HTMLSpanElement>()
+    onMounted(() => {
+      pathRef.value?.scrollTo(1000, 0)
+    })
 
     const path = computed(() => props.element.location?.path || '-')
 
@@ -151,6 +160,7 @@ export default defineComponent({
     const range = computed(() => props.element.location?.range)
 
     return {
+      pathRef,
       path,
       trimFileName,
       isExist,
@@ -163,3 +173,34 @@ export default defineComponent({
   },
 })
 </script>
+
+<style lang="scss" scoped>
+$scrollbar-width: 4px;
+
+.location {
+  overflow-x: hidden;
+
+  .path {
+    overflow-x: hidden;
+
+    span {
+      display: flex;
+      overflow-x: scroll;
+      // max-width: 100%;
+      &::-webkit-scrollbar {
+        height: $scrollbar-width;
+      }
+      &::-webkit-scrollbar-thumb {
+        background-color: rgba(128, 128, 128, 0.2);
+        border-radius: $scrollbar-width;
+      }
+      &::-webkit-scrollbar-button {
+        display: none;
+      }
+      &::-webkit-scrollbar-corner {
+        display: none;
+      }
+    }
+  }
+}
+</style>
