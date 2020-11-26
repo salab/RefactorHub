5abbf33ea8be0e700b8c28433a118570e68657c9
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-11-26 21:41:22 +0900

    fix: Fix type error

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -8,8 +8,10 @@ import {
   EditorApi,
   ElementsApi,
   ExperimentsApi,
+  Configuration,
 } from '@/apis/generated'
 
+const configuration = new Configuration()
 const basePath = '/api'
 const instance = axios.create({
   withCredentials: true,
@@ -18,14 +20,14 @@ const instance = axios.create({
 })
 
 const apis = {
-  users: new UsersApi({}, basePath, instance),
-  commits: new CommitsApi({}, basePath, instance),
-  refactorings: new RefactoringsApi({}, basePath, instance),
-  refactoringTypes: new RefactoringTypesApi({}, basePath, instance),
-  drafts: new DraftsApi({}, basePath, instance),
-  editor: new EditorApi({}, basePath, instance),
-  elements: new ElementsApi({}, basePath, instance),
-  experiments: new ExperimentsApi({}, basePath, instance),
+  users: new UsersApi(configuration, basePath, instance),
+  commits: new CommitsApi(configuration, basePath, instance),
+  refactorings: new RefactoringsApi(configuration, basePath, instance),
+  refactoringTypes: new RefactoringTypesApi(configuration, basePath, instance),
+  drafts: new DraftsApi(configuration, basePath, instance),
+  editor: new EditorApi(configuration, basePath, instance),
+  elements: new ElementsApi(configuration, basePath, instance),
+  experiments: new ExperimentsApi(configuration, basePath, instance),
 }
 
 export default apis
