a8a551193daf2e81f02b5d5e33fbd2250cedb69d
Ryo Kuramoto <kuramoto@se.cs.titech.ac.jp>
2020-12-17 21:26:19 +0900

    feat: Add temporary login button

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -4,6 +4,11 @@
       <h1 class="display-3">RefactorHub</h1>
     </div>
     <v-divider />
+    <v-row v-if="!authenticated" justify="center" class="pa-5">
+      <div class="ma-3">
+        <v-btn href="/login" class="text-none">Login</v-btn>
+      </div>
+    </v-row>
     <v-row justify="center" class="pa-5">
       <div v-for="experiment in actives" :key="experiment.id" class="ma-3">
         <v-btn :to="`/experiment/${experiment.id}`" class="text-none">
@@ -25,14 +30,20 @@ import apis, { Experiment } from '@/apis'
 
 export default defineComponent({
   setup() {
+    const authenticated = ref(true)
     const experiments = ref<Experiment[]>([])
     useAsync(async () => {
-      experiments.value = (await apis.experiments.getAllExperiments()).data
+      try {
+        await apis.users.getMe()
+        experiments.value = (await apis.experiments.getAllExperiments()).data
+      } catch {
+        authenticated.value = false
+      }
     })
 
     const actives = computed(() => experiments.value.filter((e) => e.isActive))
 
-    return { experiments, actives }
+    return { authenticated, experiments, actives }
   },
 })
 </script>
